f04411ef7f3795a40f4b7c52b88d3b94a23c06b5
Jhorman Rus <jhormanrus@gmail.com>
2021-11-14 22:31:30 -0500

    update url to object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Html5QrcodeScanner, Html5QrcodeSupportedFormats } from 'html5-qrcode';
 import * as printJS from 'print-js';
+import { Objeto } from './objeto.model';
 
 @Component({
   selector: 'app-root',
@@ -12,41 +13,61 @@ import * as printJS from 'print-js';
 export class AppComponent implements OnInit {
 
   demoForm: FormGroup
-  generatedUrl: string
+  data: string
+  decodedData: Objeto
+  invalid = false
 
-  constructor(private fb: FormBuilder, private activated: ActivatedRoute) {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.demoForm = this.fb.group({
+      one: this.fb.control('dato 1'),
+      two: this.fb.control('dato 2'),
+      three: this.fb.control('dato 3'),
+      four: this.fb.control('dato 4'),
+      five: this.fb.control('dato 5')
+    })
+
     const onScanSuccess = (decodedText) => {
       html5QrcodeScanner.clear()
-      window.location.href = decodedText
+      try {
+        this.decodedData = JSON.parse(decodedText)
+        if (this.instanceOfObjeto(this.decodedData)) {
+          this.invalid = false
+          this.demoForm.patchValue({
+            one: this.decodedData.one,
+            two: this.decodedData.two,
+            three: this.decodedData.three,
+            four: this.decodedData.four,
+            five: this.decodedData.five
+          })
+        } else {
+          this.invalid = true
+        }
+      } catch {
+        this.invalid = true
+      }
     }
     var html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250, formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE] }, false)
     html5QrcodeScanner.render(onScanSuccess, null)
-
-    this.activated.queryParams.subscribe(params => {
-      if (params.one && params.two && params.three && params.four && params.five) {
-        this.demoForm = this.fb.group({
-          one: this.fb.control(params.one),
-          two: this.fb.control(params.two),
-          three: this.fb.control(params.three),
-          four: this.fb.control(params.four),
-          five: this.fb.control(params.five)
-        })
-      } else {
-        this.demoForm = this.fb.group({
-          one: this.fb.control('dato 1'),
-          two: this.fb.control('dato 2'),
-          three: this.fb.control('dato 3'),
-          four: this.fb.control('dato 4'),
-          five: this.fb.control('dato 5')
-        })
-      }
-    })
   }
 
-  generateUrl() {
-    this.generatedUrl = `${location.origin}/?one=${encodeURIComponent(this.demoForm.get('one').value)}&two=${encodeURIComponent(this.demoForm.get('two').value)}&three=${encodeURIComponent(this.demoForm.get('three').value)}&four=${encodeURIComponent(this.demoForm.get('four').value)}&five=${encodeURIComponent(this.demoForm.get('five').value)}`
+  private instanceOfObjeto(object: any): object is Objeto {
+    return object.discriminator === 'I-AM-OBJETO'
+  }
+
+  generate() {
+    const obj: Objeto = {
+      discriminator: 'I-AM-OBJECTO',
+      one: this.demoForm.get('one').value,
+      two: this.demoForm.get('two').value,
+      three: this.demoForm.get('three').value,
+      four: this.demoForm.get('four').value,
+      five: this.demoForm.get('five').value,
+    }
+    this.data = JSON.stringify(obj)
+    console.log(this.data);
+    
   }
 
   printElem() {
---
+++ src/app/objeto.model.ts
@@ -0,0 +1,10 @@
+export interface Objeto {
+
+  discriminator: 'I-AM-OBJECTO'
+  one: string
+  two: string
+  three: string
+  four: string
+  five: string
+
+}
