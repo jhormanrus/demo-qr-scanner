5d84da5c31b0746705e0e5dc3f9a6441ae174a4a
Jhorman Rus <jhormanrus@gmail.com>
2021-11-17 18:30:00 -0500

    comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,13 @@ import { Html5Qrcode, Html5QrcodeSupportedFormats } from 'html5-qrcode';
 import * as printJS from 'print-js';
 import { Objeto } from './objeto.model';
 
+/**
+  LIBRERÍAS USADAS:
+  html5-qrcode                (importado aquí)
+  print-js                    (importado aquí)
+  @techiediaries/ngx-qrcode   (importado en app.module)
+ */ 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,15 +19,16 @@ import { Objeto } from './objeto.model';
 export class AppComponent implements OnInit {
 
   demoForm: FormGroup
-  data: string
-  decodedData: Objeto
-  invalid = false
-  notFound = false
+  data: string          //* data para generar qr
+  decodedData: Objeto   //* data extraida del scaneo de código qr
+  invalid = false       //* true si el qr escaneado es inválido (no es un objeto o no tiene el discriminador correcto 'I-AM-OBJETO')
+  notFound = false      //* true si no se concedió permisos al uso de la cámara o el dispositivo no tiene cámara
   html5QrCode: Html5Qrcode
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    //* inicializar formulario de 5 inputs
     this.demoForm = this.fb.group({
       one: this.fb.control('dato 1'),
       two: this.fb.control('dato 2'),
@@ -28,14 +36,20 @@ export class AppComponent implements OnInit {
       four: this.fb.control('dato 4'),
       five: this.fb.control('dato 5')
     })
+    //* inicializar html5QrCode con solo soporte a formato QR (formatsToSupport)
     this.html5QrCode = new Html5Qrcode("reader", { formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE], verbose: false })
+    //* inicializar scanner
     this.initScanner()
   }
 
   initScanner() {
+    //* inicializar scanner con preferencia a la camara trasera
     this.html5QrCode.start({ facingMode: 'environment' }, { fps: 10, qrbox: 250 }, (decodedText) => {
+      //* try catch si decodedText realmente es un objeto
       try {
+        //* parsear decodedText a JSON
         this.decodedData = JSON.parse(decodedText)
+        //* si decodedData contiene el discriminador 'I-AM-OBJETO', detiene el scaneo; si no, se marca inválido
         if (this.instanceOfObjeto(this.decodedData)) {
           this.html5QrCode.clear()
         } else {
@@ -45,14 +59,16 @@ export class AppComponent implements OnInit {
       } catch {
         this.invalid = true
       }
-    }, null).catch(() => this.notFound = true)
+    }, null).catch(() => this.notFound = true) //* si scanner no inicializa, se marca como cámara no encontrada
   }
 
+  //* validación si object tiene el discriminador 'I-AM-OBJETO'
   private instanceOfObjeto(object: any): object is Objeto {
     return object.discriminator === 'I-AM-OBJETO'
   }
 
   generate() {
+    //* armar obj con datos del formulario
     const obj: Objeto = {
       discriminator: 'I-AM-OBJETO',
       one: this.demoForm.get('one').value,
@@ -61,20 +77,23 @@ export class AppComponent implements OnInit {
       four: this.demoForm.get('four').value,
       five: this.demoForm.get('five').value,
     }
+    //* transformar obj a string
     this.data = JSON.stringify(obj)
   }
 
   getPermission() {
+    //* solicitar permiso al uso de la cámara
     Html5Qrcode.getCameras().then(devices => {
+      //* si cámara existe, inicializar scanner
       if (devices && devices.length) {
         this.notFound = false
-        const html5QrCode2 = new Html5Qrcode("reader", { formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE], verbose: false })
         this.initScanner()
       }
     })
   }
 
   printElem() {
+    //* imprimir div de id 2print
     printJS('2print', 'html')
   }
 }
